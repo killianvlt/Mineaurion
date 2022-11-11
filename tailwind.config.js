751ce1d108ef38e6ce5c898630125fffad61b0b7
killianvlt <killian.vaillant@gmail.com>
2022-11-11 18:09:44 +0100

    First push Nuxt Project

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  theme: {
+    colors: {
+      'dark-blue': '#232731',
+      'marine-blue': '#4F476D',
+      'blue': '#4B9AD7',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#F6EFEF',
+    },
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+      rotate: {
+        '360': '360deg',
+      }
+    }
+  },
+}
+
+
